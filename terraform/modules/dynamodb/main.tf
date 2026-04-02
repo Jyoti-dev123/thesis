ad01356635405f5ffd985547d2c289c31ac88776
Jyoti-dev123 <jyotijakhar24@gmail.com>
2026-04-02 11:48:46 +0000

    feat(dynamodb): configure model registry table with composite key (model_name + version)

--- terraform/modules/dynamodb/main.tf
+++ terraform/modules/dynamodb/main.tf
@@ -2,18 +2,11 @@ resource "aws_dynamodb_table" "model_metadata" {
   name         = "${var.name_prefix}-model-metadata"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "model_name"
+  range_key    = "version"
 
   attribute {
     name = "model_name"
     type = "S"
-  }
-
-  # Global secondary index for querying by version
-  global_secondary_index {
-    name            = "version-index"
-    hash_key        = "model_name"
-    range_key       = "version"
-    projection_type = "ALL"
   }
 
   attribute {
